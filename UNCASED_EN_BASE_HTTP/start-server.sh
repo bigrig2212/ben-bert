3003621afd1347a1085d01dbe52c77d7d2ba9f93
brig <ben@415.com>
2020-05-12 20:32:37 -0700

    tweaks

---
+++ UNCASED_EN_BASE_HTTP/start-server.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+NUMBER_OF_PROCS=$(nproc)
+NUMBER_OF_PROCS=$(($NUMBER_OF_PROCS-1))
+#bert-serving-start -model_dir models/uncased_L-12_H-768_A-12 -graph_tmp_dir ${PROJECT_HOME}/tmp  -num_worker=${NUMBER_OF_PROCS} -http_port 8125 -http_max_connect 20 -show_tokens_to_client > ${PROJECT_HOME}/logs/bert-server.log
+bert-serving-start -http_port 8125 -num_worker=${NUMBER_OF_PROCS} -model_dir models/uncased_L-12_H-768_A-12
---
+++ readme.startup.txt
@@ -0,0 +1,4 @@
+cd into UNCASED_EN_BASE_HTTP
+docker build .
+docker run ID
+docker run ID -p localhost:8125:8125 # exposes docker port 8125 on localhost 8125
