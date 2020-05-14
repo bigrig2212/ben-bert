577a152129d1f9310438d9f9572b1825871c7d84
brig <ben@415.com>
2020-05-14 11:42:25 -0700

    update

--- DockerStuff/start-server.sh
+++ DockerStuff/start-server.sh
@@ -2,4 +2,7 @@
 NUMBER_OF_PROCS=$(nproc)
 NUMBER_OF_PROCS=$(($NUMBER_OF_PROCS-1))
 #bert-serving-start -model_dir models/uncased_L-12_H-768_A-12 -graph_tmp_dir ${PROJECT_HOME}/tmp  -num_worker=${NUMBER_OF_PROCS} -http_port 8125 -http_max_connect 20 -show_tokens_to_client > ${PROJECT_HOME}/logs/bert-server.log
-bert-serving-start -http_port 8125 -num_worker=${NUMBER_OF_PROCS} -model_dir models/uncased_L-12_H-768_A-12
+#UNCASED
+#bert-serving-start -http_port 8125 -num_worker=${NUMBER_OF_PROCS} -model_dir models/uncased_L-12_H-768_A-12
+#MULTILINGUAL
+bert-serving-start -http_port 8125 -num_worker=${NUMBER_OF_PROCS} -model_dir models/multi_cased_L-12_H-768_A-12
